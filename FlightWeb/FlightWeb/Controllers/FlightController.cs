13bc97565e8fc35be0082842aec8cdfde3f62d19
wzh <wangzhaohua11@163.com>
2020-02-17 14:46:09 +0800

    1

---
+++ FlightWeb/FlightWeb/Controllers/FlightController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlightWeb.Controllers
+{
+    public class FlightController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
